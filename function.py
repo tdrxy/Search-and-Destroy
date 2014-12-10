e0822b9cb46540cd0afe1eea4ba0251a9acdd592
Thomas Dierckx <thomasdierckx@Thomass-MacBook-Pro-2.local>
2014-12-10 01:44:53 +0100

    Subdirecrs included in v2.0

--- function.py
+++ function.py
@@ -47,25 +47,30 @@ def checksum_md5(filename):
 			for chunk in iter(lambda: f.read(128*md5.block_size), b''):
 				md5.update(chunk)
 		return md5.digest()
-	except:
+	except Exception as a:
 		return -1
 
-def delete_duplicate(original, path, name):
+def delete_duplicate(original, name, prompt):
 	
 	deleted = 0
-	duplicate = path+name
-	next = False
-	while not next:
-		answer = raw_input("DUPLICATE of ["+ original+"] - DELETE [" + name + "]?(y/n) ")
-		if answer == "y":
-			deleted = os.path.getsize(duplicate)
-			#files.remove(name)
-			os.remove(duplicate)
-			next = True
-		elif answer == "n":
-			next = True
-		else:
-			next = False
+	duplicate = name
+	if prompt:
+		next = False
+		while not next:
+			answer = raw_input("DUPLICATE of ["+ original+"] - DELETE [" + name + "]?(y/n) ")
+			if answer == "y":
+				deleted = os.path.getsize(duplicate)
+				os.remove(duplicate)
+				next = True
+			elif answer == "n":
+				next = True
+			else:
+				next = False
+	else:
+		deleted = os.path.getsize(duplicate)
+		os.remove(duplicate)
+		print "DUPLICATE of ["+ original+"] - [" + name + "] - DELETED "
+
 	return deleted
 
 
@@ -73,58 +78,84 @@ def make_hashtable(files):
 
 	hashtable = {}
 	print "Making hashtable.."
-	status_bar(len(hashtable), len(files)-1)
+	status_bar(len(hashtable), len(files))
 	for f in files:
-		hashtable[f] = checksum_md5(path+f)
-		status_bar(len(hashtable)-1, len(files)-1)
+		hashtable[f] = checksum_md5(f)
+		status_bar(len(hashtable), len(files))
 	sys.stdout.write('\n')
 
 	return hashtable
 
+def get_filename(path):
+	return path.split('/')[-1]
 
-
-def search_and_destroy(files, hashtable):
+def search_and_destroy(filepaths, hashtable):
+	
 	deleted_bytes = 0
-	for original in files:
-		#print files
+	
+	for original_path in filepaths:
+
 		try:
-			
-			a_filepath = path+original
-			a_md5 = hashtable.get(original)
+		 	a_md5 = hashtable.get(original_path)
 
-			for other_name in files:
-				
-				b_filepath = path+other_name
-				b_md5 = hashtable.get(other_name)
+		 	for other_path in filepaths:
+		 		b_md5 = hashtable.get(other_path)
 
-				if a_filepath != b_filepath and a_md5 == b_md5 and a_md5 != -1 and b_md5 != -1:
-					deleted_bytes += delete_duplicate(original, path, other_name)
-					
+		 		if original_path != other_path and a_md5 == b_md5 and a_md5 != -1 and b_md5 != -1:
+		 			
+		 			#Are the names equal but the paths different? -> Duplicate, no prompt.
+		 			if(get_filename(original_path) == get_filename(other_path)):
+		 				deleted_bytes += delete_duplicate(original_path, other_path, prompt=False)
+		 			#Are you sure they are duplicate? Uncertainty requires prompt = True
+		 			else:
+		 				deleted_bytes += delete_duplicate(original_path, other_path, prompt=False)
 
 		except IOError as exc:
-			print exc
-			#pass
-		except:
-			pass
+		 	pass
+		except Exception as a:
+		 	pass
 
-		files.remove(original)
 	return deleted_bytes
 
-def __main__():
-	COLS = struct.unpack('hh',  fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))[1]
-	path = '/Users/thomasdierckx/Documents/scripts/'   
-	print "___________________________________________________________"
-	print "Working dir: "+ path
 
-	files = os.listdir(path)
-	hashtable = make_hashtable(files)
-	deleted_bytes = search_and_destroy(files, hashtable)
+##############
+#   PATH
+path = '/Users/thomasdierckx/Desktop/'
+extensions = ["jpg", "jpeg", "png", "img", "img", "wav", "mp3", "mov", "vid", "m4a"]
+#
+###############
 
-	if deleted_bytes == 0:
-		print bold("No Duplicates.")
+COLS = struct.unpack('hh',  fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))[1]
+   
+print "___________________________________________________________"
+print "Working dir: "+ path
+
+#Get subdirectories
+directories = []
+for directory in [x[0] for x in os.walk(path)]:
+	if directory[-1] != "/":
+		directory = directory +"/"
+		directories.append(directory)
 	else:
-		print bold("Freed "+str(deleted_bytes/1024)+"Kb of "+ str(os.path.getsize(path)) +"Kb.")
+		directories.append(directory)
+
+#Get all files in all directories/subdirectories
+files = []
+for directory in directories:
+	names = os.listdir(directory)
+	files += [directory+name for name in names]
+
+files = [f for f in files if f.split('.')[-1].lower() in extensions]
+hashtable = make_hashtable(files)
+deleted_bytes = search_and_destroy(files, hashtable)
 
 
-	print "___________________________________________________________"
+
+if deleted_bytes == 0:
+	print bold("No Duplicates.")
+else:
+	print bold("Freed "+str(deleted_bytes/(1024*1024))+"Mb.")
+
+
+print "___________________________________________________________"
 
